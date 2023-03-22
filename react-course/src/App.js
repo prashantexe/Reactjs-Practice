6c6d12cb39c9ad4e3271eb0f79d239a961e61c12
Prashant <prashantd049@gmail.com>
2023-03-22 11:09:21 +0530

    Commit

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <h3>Sumbit</h3>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
     </div>
   );
 }
